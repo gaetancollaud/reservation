0c62cc638d66084050825550a5bda4638e7d1f41
Gaétan Collaud <gaetancollaud@gmail.com>
2020-01-18 10:59:51 +0100

    multiple formation

--- src/main/java/net/collaud/gaetan/reservation/service/ReservationService.java
+++ src/main/java/net/collaud/gaetan/reservation/service/ReservationService.java
@@ -22,81 +22,84 @@ import java.util.Optional;
 @Transactional
 @Service
 public class ReservationService {
-	@Autowired
-	private ReservationRepository reservationRepository;
+    @Autowired
+    private ReservationRepository reservationRepository;
 
-	@Autowired
-	private ResourceRepository resourceRepository;
+    @Autowired
+    private ResourceRepository resourceRepository;
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@Autowired
-	private CalendarService calendarService;
+    @Autowired
+    private CalendarService calendarService;
 
-	public Reservation addReservation(Reservation reservation) {
-		reservation.setUser(userService.getCurrentUserOrThrow());
-		checkConflict(reservation);
-		checkReservationRestriction(reservation);
-		return reservationRepository.save(reservation);
-	}
+    public Reservation addReservation(Reservation reservation) {
+        reservation.setUser(userService.getCurrentUserOrThrow());
+        checkConflict(reservation);
+        checkReservationRestriction(reservation);
+        return reservationRepository.save(reservation);
+    }
 
-	public Reservation editReservation(Reservation reservation) {
-		Reservation old = checkSecurity(reservation.getId());
-		checkConflict(reservation);
-		checkReservationRestriction(reservation);
+    public Reservation editReservation(Reservation reservation) {
+        Reservation old = checkSecurity(reservation.getId());
 
-		// update the updatable field
-		old.setResource(reservation.getResource());
-		old.setTimestampStart(reservation.getTimestampStart());
-		old.setTimestampEnd(reservation.getTimestampEnd());
+        // update the updatable field
+        old.setResource(reservation.getResource());
+        old.setTimestampStart(reservation.getTimestampStart());
+        old.setTimestampEnd(reservation.getTimestampEnd());
 
-		return reservationRepository.save(old);
-	}
+        checkConflict(reservation);
+        checkReservationRestriction(reservation);
 
-	public void deleteReservation(long reservationId) {
-		checkSecurity(reservationId);
-		reservationRepository.deleteById(reservationId);
-	}
+        return reservationRepository.save(old);
+    }
 
-	protected void checkConflict(Reservation reservation) {
-		Optional<Reservation> inConflict = reservationRepository.search(new ReservationCriteria(
-			reservation.getTimestampStart(),
-			reservation.getTimestampEnd(),
-			reservation.getResource().getId())).stream()
-			.filter(r -> !r.getId().equals(reservation.getId()))
-			.findFirst();
+    public void deleteReservation(long reservationId) {
+        checkSecurity(reservationId);
+        reservationRepository.deleteById(reservationId);
+    }
 
-		inConflict.ifPresent(conflict -> {
-			throw new ReservationConflictException("Conflict found with reservation " + conflict.getId());
-		});
-	}
+    protected void checkConflict(Reservation reservation) {
+        long inConflict = reservationRepository.search(new ReservationCriteria(
+            reservation.getTimestampStart(),
+            reservation.getTimestampEnd(),
+            reservation.getResource().getId())).stream()
+            .filter(r -> !r.getId().equals(reservation.getId()))
+            .count();
 
-	protected Reservation checkSecurity(long reservationId) {
-		Reservation old = reservationRepository.getOne(reservationId);
-		if (SecurityUtils.isCurrentUserInRole(AuthoritiesConstants.RESERVATION_MANAGE)) {
-			return old;
-		}
-		User currentUser = userService.getCurrentUserOrThrow();
-		if (!currentUser.getId().equals(old.getUser().getId())) {
-			throw new RuntimeException("You cannot edit the reservation of others");
-		}
-		return old;
-	}
+        Resource resource = resourceRepository.getOne(reservation.getResource().getId());
 
-	protected void checkReservationRestriction(Reservation reservation) {
-		Resource resource = resourceRepository.getOne(reservation.getResource().getId());
-		if (!StringUtils.isEmpty(resource.getCalendarLink())) {
-			Optional<String> regex = Optional.ofNullable(resource.getCalendarSearchRegex())
-				.filter(v -> !StringUtils.isEmpty(v));
-			Interval interval = new Interval(reservation.getTimestampStart().toInstant(), reservation.getTimestampEnd().toInstant());
+        if (inConflict + 1 > resource.getType().getMaxResource()) {
+            throw new ReservationConflictException("Conflict found with " + inConflict + " reservation(s)");
+        }
+    }
 
-			if (!calendarService.isAllowed(
-				resource.getCalendarLink(),
-				regex,
-				interval)) {
-				throw new ResourceLimitationException("Unable to book because there is a calendar restriction");
-			}
-		}
-	}
+    protected Reservation checkSecurity(long reservationId) {
+        Reservation old = reservationRepository.getOne(reservationId);
+        if (SecurityUtils.isCurrentUserInRole(AuthoritiesConstants.RESERVATION_MANAGE)) {
+            return old;
+        }
+        User currentUser = userService.getCurrentUserOrThrow();
+        if (!currentUser.getId().equals(old.getUser().getId())) {
+            throw new RuntimeException("You cannot edit the reservation of others");
+        }
+        return old;
+    }
+
+    protected void checkReservationRestriction(Reservation reservation) {
+        Resource resource = resourceRepository.getOne(reservation.getResource().getId());
+        if (!StringUtils.isEmpty(resource.getCalendarLink())) {
+            Optional<String> regex = Optional.ofNullable(resource.getCalendarSearchRegex())
+                .filter(v -> !StringUtils.isEmpty(v));
+            Interval interval = new Interval(reservation.getTimestampStart().toInstant(), reservation.getTimestampEnd().toInstant());
+
+            if (!calendarService.isAllowed(
+                resource.getCalendarLink(),
+                regex,
+                interval)) {
+                throw new ResourceLimitationException("Unable to book because there is a calendar restriction");
+            }
+        }
+    }
 }
